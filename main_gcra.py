53099ad1f3acff623e856ea46fbb204d6ee51c2b
Andrea Stagi <stagi.andrea@gmail.com>
2020-04-27 15:06:21 +0200

    Add lock to GCRA

--- main_gcra.py
+++ main_gcra.py
@@ -4,10 +4,10 @@ from ratelimit import gcra
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-requests = 25
+requests = 10
 
 for i in range(requests):
-    if gcra.request_is_limited(r, 'admin', 20, timedelta(seconds=30)):
+    if gcra.request_is_limited(r, 'admin', 10, timedelta(minutes=1)):
         print ('🛑 Request is limited')
     else:
         print ('✅ Request is allowed')
--- ratelimit/gcra.py
+++ ratelimit/gcra.py
@@ -5,11 +5,15 @@ from redis import Redis
 def request_is_limited(r: Redis, key: str, limit: int, period: timedelta):
     period_in_seconds = int(period.total_seconds())
     t = r.time()[0]
-    separation =  period_in_seconds / limit
+    separation = round(period_in_seconds / limit)
     r.setnx(key, 0)
-    tat = max(int(r.get(key)), t)
-    if tat - t <= period_in_seconds - separation:
-        new_tat = max(tat, t) + separation
-        r.set(key, new_tat)
-        return False
-    return True
+    try:
+        with r.lock('lock:' + key, blocking_timeout=5) as lock:
+            tat = max(int(r.get(key)), t)
+            if tat - t <= period_in_seconds - separation:
+                new_tat = max(tat, t) + separation
+                r.set(key, new_tat)
+                return False
+            return True
+    except LockError:
+        return True
